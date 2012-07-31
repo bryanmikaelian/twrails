5b839a18f5095fe2ba40dad3ce9755d2ae6d31f9
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2012-07-31 12:26:59 -0500

    Basic home controller.  Callback goes to the index action on the home controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,15 @@
 class SessionsController < ApplicationController
 
   def create
+    session[:twitter_access_token] = request.env['omniauth.auth']['credentials']['token']
+    session[:twitter_access_secret] = request.env['omniauth.auth']['credentials']['secret']
+    redirect_to home_path
   end
 
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Twrails::Application.routes.draw do
   root :to => 'sessions#index'
+
+  match 'auth/twitter/callback', to: 'sessions#create'
+  match '/home', :to => 'home#index'
 end
